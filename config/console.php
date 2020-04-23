963501a1ed2882801e6fa297c1286e1e09495458
Andrew <andrew.panix@gmail.com>
2020-04-24 00:18:23 +0300

    init

--- config/console.php
+++ config/console.php
@@ -17,10 +17,11 @@ $config = [
     ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=corner.mysql.tools;dbname=corner_bot2',
-            'username' => 'corner_bot2',
-            'password' => 'k4-6H3ds_K',
+            'dsn' => 'mysql:host=localhost;dbname=',
+            'username' => '',
+            'password' => '',
             'tablePrefix' => 'prefix_',
+            'charset' =>'utf8'
         ],
     ],
     'params' => yii\helpers\ArrayHelper::merge([
--- config/web.php
+++ config/web.php
@@ -12,11 +12,11 @@ $config = [
     ],
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=corner.mysql.tools;dbname=corner_bot2',
-            'username' => 'corner_bot2',
-            'password' => 'k4-6H3ds_K',
+            'dsn' => 'mysql:host=localhost;dbname=',
+            'username' => '',
+            'password' => '',
             'tablePrefix' => 'prefix_',
-			'charset' =>'utf8mb4'
+			'charset' =>'utf8'
         ],
         'request' => [
             'cookieValidationKey' => 'fpsiKaSs1Mcb6zwlsUZwuhqScBs5UgPQ',
